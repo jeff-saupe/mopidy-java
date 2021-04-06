746f57f2d79b20c5b88aa7d9eaa690f09f976894
Jeff Saupe <saupe.jeff@gmail.com>
2021-04-06 17:49:00 +0200

    Update Main.java

--- src/main/java/danbroid/mopidy/Main.java
+++ src/main/java/danbroid/mopidy/Main.java
@@ -1,6 +1,7 @@
 package danbroid.mopidy;
 
 import danbroid.mopidy.interfaces.CallContext;
+import danbroid.mopidy.model.TlTrack;
 import danbroid.mopidy.model.Track;
 
 public class Main {
@@ -12,14 +13,34 @@ public class Main {
         connection.setURL("192.168.178.60", 6680);
         connection.start();
 
+        connection.getTracklist().clear().call(new ResponseHandler<Void>() {
+            @Override
+            public void onResponse(CallContext context, Void result) {
+                System.out.println("Cleared");
+            }
+        });
+        connection.getTracklist().add("spotify:track:6RIYfiZrX1ZkbDDOqOOGWM").call(new ResponseHandler<TlTrack[]>() {
+            @Override
+            public void onResponse(CallContext context, TlTrack[] result) {
+                if (result != null)
+                    System.out.println("added");
+            }
+        });
+        connection.getPlayback().play(1).call(new ResponseHandler<Void>() {
+            @Override
+            public void onResponse(CallContext context, Void result) {
+                System.out.println("Playing");
+            }
+        });
 
         connection.getPlayback().getCurrentTrack().call(new ResponseHandler<Track>() {
             @Override
             public void onResponse(CallContext context, Track result) {
-
-                System.out.println(result.getName());
+                if (result != null)
+                    System.out.println(result.getName());
             }
         });
+
     }
 
 }
