e25e1a0b9c6ae9e490dd575a6151ec40a23f0ab4
Jeff Saupe <saupe.jeff@gmail.com>
2021-04-12 19:47:12 +0200

    Update Mixer.java

--- src/main/java/danbroid/mopidy/api/Mixer.java
+++ src/main/java/danbroid/mopidy/api/Mixer.java
@@ -7,44 +7,48 @@ import org.slf4j.LoggerFactory;
 
 @Slf4j
 public class Mixer extends Api {
-	protected Mixer(Api parent) {
-		super(parent, "mixer.");
-	}
+    protected Mixer(Api parent) {
+        super(parent, "mixer.");
+    }
 
-	/**
-	 * Get the volume.
-	 * @return Integer in range [0..100] or :class:`None` if unknown
-	 */
-	public Call<Integer> getVolume() {
-		return createCall("get_volume", Integer.class);
-	}
+    /**
+     * Get the volume.
+     *
+     * @return Integer in range [0..100] or Null if unknown
+     */
+    public Call<Integer> getVolume() {
+        return createCall("get_volume", Integer.class);
+    }
 
-	/**
-	 * Set the volume.
-	 * @param volume Integer in range [1..100]
-	 * @return Returns :class:`True` if call is successful, otherwise :class:`False`
-	 */
-	public Call<Boolean> setVolume(int volume) {
-		return createCall("set_volume", Boolean.class)
-				.addParam("volume", volume);
-	}
+    /**
+     * Set the volume.
+     *
+     * @param volume Integer in range [1..100]
+     * @return Returns True if call is successful, otherwise False
+     */
+    public Call<Boolean> setVolume(int volume) {
+        return createCall("set_volume", Boolean.class)
+                .addParam("volume", volume);
+    }
 
-	/**
-	 * Get mute state.
-	 * @return :class:`True` if muted, :class:`False` unmuted, :class:`None` if unknown
-	 */
-	public Call<Boolean> getMute() {
-		return createCall("get_mute", Boolean.class);
-	}
+    /**
+     * Get mute state.
+     *
+     * @return True if muted, False if unmuted, Null if unknown
+     */
+    public Call<Boolean> getMute() {
+        return createCall("get_mute", Boolean.class);
+    }
 
-	/**
-	 * Set mute state.
-	 * @param mute :class:`True` to mute, :class:`False` to unmute.
-	 * @param successHandler Handler
-	 * @return :class:`True` if call is successful, otherwise :class:`False`.
-	 */
-	public Call<Boolean> setMute(boolean mute, ResponseHandler<Boolean> successHandler) {
-		return createCall("set_mute", Boolean.class)
-				.addParam("mute", mute);
-	}
+    /**
+     * Set mute state.
+     *
+     * @param mute           True to mute, False to unmute.
+     * @param successHandler Handler
+     * @return True if call is successful, otherwise False.
+     */
+    public Call<Boolean> setMute(boolean mute, ResponseHandler<Boolean> successHandler) {
+        return createCall("set_mute", Boolean.class)
+                .addParam("mute", mute);
+    }
 }
